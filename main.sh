f9b23b94a8784abe9294054df3c3ad1d1c953c14
Zani Felipe <62755729+FelipeZani@users.noreply.github.com>
2023-12-14 16:13:26 +0100

    Functs to compute execution time
    I added two fonctions to compute the execution time of any task.
    getTime():  gets the local time of the user and convert it to seconds
    taskDuration(): computes the ellapsed time

--- main.sh
+++ main.sh
@@ -7,56 +7,88 @@ checkDir() #function which will verify if the directory indicated by the user ex
         echo "Directory not found"
         exit 1
     fi
+	
     csv_file=$1
-    if [ ! -f "$csv_file/data.csv" ]
+    
+	if [ ! -f "$csv_file/data.csv" ]
     then
         echo "File not found"
     fi
    
 }
-checkEOut()
+
+checkEOut() #verify if the executable exists
 {
+
     cur_dir=`dirname pwd`/progc
-    E_out=0 #does exist .out?
-    cd "$cur_dir"
+    E_out=0 
+	
+    cd $cur_dir
     
-    for entry in *
+    for entry in * #search for an executable inside the progc folder
     do
-        if [ -x "$entry"  ]
+        if [ -x $entry  ]
         then
             E_out=1
         fi
     done
-    if [ $E_out -eq 0 ]
+	
+    if [ $E_out -eq 0 ] 
     then
         `make`
+		return_make=$?
+		if [ $return_make -ne 0 ]#verification of the return of make command
+		then
+			echo "Program failled to build an executable"
+			exit 1
+		fi
     fi
+	
     cd ..
+	
 }
 
-if [ "$#" -eq 0 ]; then #check if any parameter was passed
+getTime() #this function gets the local time of the computer and transforms it in seconds
+{
+
+	hour=`date +%H`
+	minutes=`date +%M`
+	secondes=`date +%S`
+	tot_sec=$((${hour}*3600 + ${minutes}*60 + ${secondes}))
+	
+	echo "$tot_sec"
+
+}
+
+taskDuration()#this function will receive an initial time passed by argument and computate the duration of execution of a certain task
+{
+
+	init_time=$1
+	end_time=$(getTime)
+	dur_exec=$((${end_time} - ${init_time}))
+	
+	echo $dur_exec
+}
+
+if [ $# -eq 0 ]; then #check if any parameter was passed
     echo "No arguments provided"
     exit 1
 fi
 
-all_args=$* 
+all_args=$*
 input_dir=$1
 
-
-checkDir "$input_dir"
+checkDir $input_dir
 checkEOut 
 
-for i in $all_args
+for i in $all_args #print the help list
 do
-    if [ "$i" = "-h" ]  #je comprends pas comme pq -eq et == marche pas mais = magiquement marche
+    if [ "$i" = "-h" ]
     then
-        echo "all future command list"
+        echo "future: command list"
         exit 0
     fi
 done
-
-
-
 
 
 
